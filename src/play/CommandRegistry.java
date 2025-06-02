35e8f80c4cb1dd2d4f6f521e4508123f75474ffb
sachaloskov <sacha.loskov@gmail.com>
2025-06-02 11:07:46 +0200

    Style v3

--- src/play/CommandRegistry.java
+++ src/play/CommandRegistry.java
@@ -27,9 +27,9 @@ public class CommandRegistry {
 
         if (command == null) {
             return StringStyling.StyleStringBright("Unknown command: " + keyword, Style.BOLD, Color.WHITE, Color.RED);
+        }
 
         return command.execute(args);
-        }
     }
 
     public Map<String, Command> getCommands() {
